6cfebc9b71c25050c5dd770e32f0dcd2b4db267e
Alexander Hurd <hurdad@gmail.com>
2013-11-03 01:11:37 -0500

    adding postinst scrits

--- contrib/package/deb/qfs-chunkserver.build.sh
+++ contrib/package/deb/qfs-chunkserver.build.sh
@@ -24,9 +24,10 @@ mkdir -p $QFS_BUILD_ROOT$NAME"_"$VERSION$QFS_INSTALL_PREFIX/conf
 install -m 755 $SOURCE_DIR/qfs/build/release/bin/chunk* $QFS_BUILD_ROOT$NAME"_"$VERSION$QFS_INSTALL_PREFIX/bin
 install -m 644 $SOURCE_DIR/qfs/conf/ChunkServer.prp $QFS_BUILD_ROOT$NAME"_"$VERSION$QFS_INSTALL_PREFIX/conf
 
-#ADD DEBIAN/control
+#ADD DEBIAN/control & postinst
 mkdir -p $QFS_BUILD_ROOT$NAME"_"$VERSION/DEBIAN
 cp $SOURCE_DIR/qfs/contrib/package/deb/qfs-chunkserver.DEBIAN.control $QFS_BUILD_ROOT$NAME"_"$VERSION/DEBIAN/control
+cp $SOURCE_DIR/qfs/contrib/package/deb/qfs-chunkserver.DEBIAN.postinst $QFS_BUILD_ROOT$NAME"_"$VERSION/DEBIAN/postinst
 
 #MODIFY
 sudo chown -R root:root $QFS_BUILD_ROOT$NAME"_"$VERSION
